f7535be77485a9ded1037251ddc5c0fb1fbcfc88
Alex <alexalford2@gmail.com>
2024-06-05 18:16:01 -0700

    Update Badge.tsx
    Changed branding back to Powered by Sophosic.

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -48,17 +48,17 @@ export const Badge = (props: Props) => {
             'background-color': props.badgeBackgroundColor ?? '#ffffff',
           }}
         >
-          {props.footer?.text ?? 'Powered by'}
+          {props.footer?.text ?? 'Powered by '}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://flowiseai.com'}
+            href={props.footer?.companyLink ?? 'https://sophosic.ai'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
             id="lite-badge"
             style={{ 'font-weight': 'bold', color: props.footer?.textColor ?? props.poweredByTextColor ?? defaultTextColor }}
           >
-            <span>&nbsp;{props.footer?.company ?? 'Flowise'}</span>
+            <span>&nbsp;{props.footer?.company ?? 'Sophosic'}</span>
           </a>
         </span>
       </Show>
